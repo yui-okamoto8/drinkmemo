9a74da05173a11e1f608678147c2922bbc7353bf
USER <user@USERnoMacBook-Air.local>
2026-02-15 23:27:49 +0900

    キーワード検索バーとページ移動作成

--- drinmemo/drink_app/views.py
+++ drinmemo/drink_app/views.py
@@ -1,13 +1,35 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import DrinkRecordForm
+from django.db.models import Q
+from django.core.paginator import Paginator
+
 from .models import DrinkRecord
 
 
 @login_required
 def drink_list(request):
+    query = request.GET.get('q')
+
     records = DrinkRecord.objects.filter(user=request.user).order_by('-recorded_date', '-id')
-    return render(request, 'drink_app/drink_list.html',{"records": records})
+    
+    if query:
+        records = records.filter(
+            Q(drink_name__icontains=query) |
+            Q(store_name__icontains=query) |
+            Q(maker_name__icontains=query)
+        )
+  
+    paginator = Paginator(records, 2)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, 'drink_app/drink_list.html',{
+        'records': records,
+        'page_obj': page_obj,
+        'q': query,
+        })
+
 
 @login_required 
 def drink_create(request):
