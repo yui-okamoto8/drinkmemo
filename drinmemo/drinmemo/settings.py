f352466822719d5eb121d6179596664f2f39e763
USER <user@USERnoMacBook-Air.local>
2026-02-19 01:10:12 +0900

    ホーム画面修正

--- drinmemo/drinmemo/settings.py
+++ drinmemo/drinmemo/settings.py
@@ -124,7 +124,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
