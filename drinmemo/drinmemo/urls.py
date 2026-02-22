d9cb49c4b6fef1777bb332cdff538e5243168fd4
USER <user@USERnoMacBook-Air.local>
2026-02-22 19:36:25 +0900

    トップページ修正

--- drinmemo/drinmemo/urls.py
+++ drinmemo/drinmemo/urls.py
@@ -2,13 +2,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import redirect
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('drink/', include('drink_app.urls')),
-    path('portfolio/', include('portfolio.urls')),
+    path('portfolio/', include('portfolio.urls')),\
+    path('', lambda request: redirect('portfolio:top')),
 ]
 
 if settings.DEBUG:
